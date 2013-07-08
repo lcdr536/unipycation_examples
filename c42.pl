89af548138cdc916620973071fde5f06684cc45c
vext01 <vext01@gmail.com>
2013-07-08 14:26:30 +0100

    Only search where a counter is.
    Makes this much faster.

--- c42.pl
+++ c42.pl
@@ -11,42 +11,27 @@ search_vector(COINS, c(X, Y), v(XD, YD), CT) :-
 	NEXT_Y is Y + YD,
 	search_vector(COINS, c(NEXT_X, NEXT_Y), v(XD, YD), NEXT_CT), !.
 
-find_consecutive(COINS, X, Y) :-
+find_consecutive(COINS, c(X, Y)) :-
 	search_vector(COINS, c(X, Y), v(1, 0), 0), % search right
 	format("horizontal win at (~p, ~p)~n", [X, Y]), !.
-find_consecutive(COINS, X, Y) :-
+find_consecutive(COINS, c(X, Y)) :-
 	search_vector(COINS, c(X, Y), v(0, 1), 0), % search down
 	format("vertical win at (~p, ~p)~n", [X, Y]), !.
-find_consecutive(COINS, X, Y) :-
+find_consecutive(COINS, c(X, Y)) :-
 	search_vector(COINS, c(X, Y), v(1, 1), 0), % search lr diag
 	format("LR-diagonal win at (~p, ~p)~n", [X, Y]), !.
-find_consecutive(COINS, X, Y) :-
+find_consecutive(COINS, c(X, Y)) :-
 	search_vector(COINS, c(X, Y), v(-1, 1), 0), % search rl diag
 	format("RL-diagonal win at (~p, ~p)~n", [X, Y]), !.
 
-search(COINS, X, Y) :-
-	board_height(H), Y < H,
-	board_width(W), X < W,
-	find_consecutive(COINS, X, Y), !. % There is a winning run starting here
-
-search(COINS, X, Y) :-
-	board_height(H), Y < H,
-	board_width(W), X < W,
-	NEXT_X is  X + 1,
-	search(COINS, NEXT_X, Y), !. % Search one space right
-
-search(COINS, X, Y) :-
-	board_height(H), Y < H,
-	board_width(W), X < W,
-	NEXT_Y is Y + 1,
-	search(COINS, 0, NEXT_Y), !. % search starting from beginning of next row
-
 has_won(REDS, _, WHO) :-
-	search(REDS, 0, 0),
+	member(C, REDS),
+	find_consecutive(REDS, C),
 	WHO = red, !.
 
 has_won(_, YELLOWS, WHO) :-
-	search(YELLOWS, 0, 0),
+	member(C, YELLOWS),
+	find_consecutive(YELLOWS, C),
 	WHO = yellow, !.
 
 % Test case
