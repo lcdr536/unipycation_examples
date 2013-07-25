bfeb7f126d9e3b8885c40ba1d7f012d85f7ede58
vext01 <vext01@gmail.com>
2013-07-25 12:30:32 +0100

    Finally, fix this test. Jeez....

--- poker/tests.pl
+++ poker/tests.pl
@@ -5,17 +5,15 @@
 test(straight_flush) :-
 	Hand = [ card(7, c), card(8, c), card(9, c), card(10, c), card(j, c) ],
 	findall(res(HandName, Match), hand(Hand, HandName, Match), Results),
-	%format("~p\n", Results),
 	member(res(straight_flush,  Hand), Results).
 
 test(four_of_a_kind) :-
-	%Hand = [ card(7, c), card(7, h), card(7, d), card(7, s) ],
-	findall(res(HandName, Match), hand([card(7, c), card(7, h), card(7, d), card(7, s)], HandName, Match), Results),
-	format("~p\n", Results).
-	%member(res(four_of_a_kind,  Hand), Results).
+	Hand = [ card(7, c), card(7, h), card(7, d), card(7, s) ],
+	findall(res(HandName, Match), hand(Hand, HandName, Match), Results),
+	format("~k\n", [Results]),
+	memberchk(res(four_of_a_kind,  Hand), Results).
 
 test(high_card, all(Match == [[card(4, s)]])) :-
 	hand([card(4, s)], high_card, Match).
-
 
 :- end_tests(poker).
