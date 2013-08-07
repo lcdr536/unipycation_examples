5d06a4752d127acc3d1d2dbbad35279fb35a6471
Carl Friedrich Bolz <cfbolz@gmx.de>
2013-08-07 18:17:03 +0200

    some more microbenchmarks

--- microbenchmarks/microbenchmarks.py
+++ microbenchmarks/microbenchmarks.py
@@ -25,7 +25,74 @@ countdown_using_multiply_and_add(N) :-
     N > 0,
     multiply_and_add(N, 1, -1, N0),
     countdown_using_multiply_and_add(N0).
+
+countdown_using_multiply_and_add_in_python(0).
+countdown_using_multiply_and_add_in_python(N) :-
+    N > 0,
+    python:multiply_and_add(N, 1, -1, N0),
+    countdown_using_multiply_and_add_in_python(N0).
+
+gen1(X, 1) :- X >= 0.
+gen1(X, Y) :- X > 0, X0 is X - 1, gen1(X0, Y).
+
+call_gen1_n_times(0, _).
+call_gen1_n_times(Iterations, Count) :-
+    Iterations > 0,
+    ((gen1(Count, X), X == 1, fail); true),
+    Iterations0 is Iterations - 1,
+    call_gen1_n_times(Iterations0, Count).
+
+make_chain(A, B) :- make_chain(A, end, B).
+make_chain(X, In, Out) :-
+    X >= 0,
+    X0 is X - 1,
+    X2 is X * 2,
+    make_chain(X0, pair(X, X2, In), Out).
+make_chain(0, In, In).
+
+consume_chain(end, X, X).
+consume_chain(pair(X, X2, Next), In, Out) :-
+    In1 is In + X2 - X,
+    consume_chain(Next, In1, Out).
+
+make_and_consume_chain_n_times(0, _).
+make_and_consume_chain_n_times(N, X) :-
+    N > 0, make_chain(X, Chain), consume_chain(Chain, 0, _),
+    N0 is N - 1, make_and_consume_chain_n_times(N0, X).
+
+make_list(A, B) :- make_list(A, [], B).
+make_list(X, In, Out) :-
+    X >= 0,
+    X0 is X - 1,
+    make_list(X0, [X|In], Out).
+make_list(0, In, In).
+
+consume_list([], X, X).
+consume_list([X | Next], In, Out) :-
+    In1 is In + X,
+    consume_list(Next, In1, Out).
+
+make_and_consume_list_n_times(0, _).
+make_and_consume_list_n_times(N, X) :-
+    N > 0, make_list(X, List), consume_list(List, 0, _),
+    N0 is N - 1, make_and_consume_list_n_times(N0, X).
+
+consume_instchain(Inst, X, X) :- Inst:is_terminator(1).
+consume_instchain(Inst, In, Out) :-
+    Inst:is_terminator(0),
+    Inst:get_value(X),
+    In1 is In + X,
+    Inst:get_next(Next),
+    consume_instchain(Next, In1, Out).
+
+
+make_and_consume_instchain_n_times(0, _).
+make_and_consume_instchain_n_times(N, X) :-
+    N > 0, python:make_instchain(X, List), consume_instchain(List, 0, _),
+    N0 is N - 1, make_and_consume_instchain_n_times(N0, X).
 """)
+
+str = str
 
 def multiply_and_add(a, b, c):
     return a + b * c
@@ -41,7 +108,90 @@ def sum_up_to_n(n):
         n -= 1
     return result
 
+def make_chain(x):
+    curr = "end"
+    while x >= 0:
+        curr = ("pair", x, 2*x, curr)
+        x -= 1
+    return curr
+
+def consume_chain(chain):
+    res = 0
+    while chain != "end":
+        tag, val, val2, chain = chain
+        assert tag == "pair"
+        res += val2 - val
+    return res
+
+def consume_chain_prolog(chain):
+    res = 0
+    while chain != "end":
+        assert chain.name == "pair"
+        val, val2, chain = chain[0], chain[1], chain[2]
+        res += val2 - val
+    return res
+
+
+class Terminator(object):
+    def is_terminator(self):
+        return True
+
+class Chain(object):
+    def __init__(self, value, next):
+        self._value = value
+        self._next = next
+
+    def is_terminator(self):
+        return False
+
+    def get_value(self):
+        return self._value
+
+    def get_next(self):
+        return self._next
+
+def make_instchain(x):
+    curr = Terminator()
+    while x >= 0:
+        curr = Chain(x, curr)
+        x -= 1
+    return curr
+
+def consume_instchain(chain):
+    res = 0
+    while not chain.is_terminator():
+        res += chain.get_value()
+        chain = chain.get_next()
+    return res
+
+def make_list(x):
+    return range(x, 0, -1)
+
+def consume_list(l):
+    res = 0
+    for i in l:
+        res += i
+    return res
+
+def gen1(x):
+    while x >= 0:
+        yield 1
+        x -= 1
+
+# __________________________________________________________
+
+class BenchMeta(type):
+    all_classes = []
+
+    def __new__(cls, name, bases, dct):
+        res = type.__new__(cls, name, bases, dct)
+        if name != "Bench":
+            BenchMeta.all_classes.append(res)
+        return res
+
 class Bench(object):
+    __metaclass__ = BenchMeta
+
     def __init__(self, args):
         self.iterations = args.iterations
         self.count = args.count
@@ -55,7 +205,36 @@ class Bench(object):
         print self.run()
 
 class TinyFunc(Bench):
-    name = "calling a tiny function in prolog with 3 arguments and 1 result"
+    name = "calling a tiny prolog function with 3 arguments and 1 result"
+
+    def prolog(self, iterations, count):
+        iterations_countdown = iterations * count
+        t1 = time.time()
+        res = e.db.countdown_using_multiply_and_add(iterations_countdown)
+        assert res == ()
+        t2 = time.time()
+        return t2 - t1
+
+    def both(self, iterations, count):
+        iterations_countdown = iterations * count
+        t1 = time.time()
+        i = iterations_countdown
+        while i > 0:
+            i, = e.db.multiply_and_add(i, 1, -1, None)
+        t2 = time.time()
+        return t2 - t1
+
+    def python(self, iterations, count):
+        iterations_countdown = iterations * count
+        t1 = time.time()
+        i = iterations_countdown
+        while i > 0:
+            i = multiply_and_add(i, 1, -1)
+        t2 = time.time()
+        return t2 - t1
+
+class TinyFuncFromProlog(Bench):
+    name = "calling a tiny python function from Prolog with 3 arguments and 1 result"
 
     def prolog(self, iterations, count):
         iterations_countdown = iterations * count
@@ -67,9 +246,7 @@ class TinyFunc(Bench):
     def both(self, iterations, count):
         iterations_countdown = iterations * count
         t1 = time.time()
-        i = iterations_countdown
-        while i > 0:
-            i, = e.db.multiply_and_add(i, 1, -1, None)
+        e.db.countdown_using_multiply_and_add_in_python(iterations_countdown)
         t2 = time.time()
         return t2 - t1
 
@@ -130,6 +307,102 @@ class Loop1Arg1Result(Bench):
         t2 = time.time()
         return t2 - t1
 
+class NondetLoop1Arg1Result(Bench):
+    name = "calling a prolog predicate with 1 arguments and 1 result, with many solutions"
+    def prolog(self, iterations, count):
+        t1 = time.time()
+        e.db.call_gen1_n_times(iterations, count)
+        t2 = time.time()
+        return t2 - t1
+    def both(self, iterations, count):
+        e.db.gen1.many_solutions = True
+        t1 = time.time()
+        for i in range(iterations):
+            for i, in e.db.gen1(count, None):
+                assert i == 1
+        t2 = time.time()
+        return t2 - t1
+    def python(self, iterations, count):
+        t1 = time.time()
+        for i in range(iterations):
+            for i in gen1(count):
+                assert i == 1
+        t2 = time.time()
+        return t2 - t1
+
+class Terms(Bench):
+    name = "producing and consuming Prolog terms"
+    def prolog(self, iterations, count):
+        t1 = time.time()
+        res = e.db.make_and_consume_chain_n_times(iterations, count)
+        assert res == ()
+        t2 = time.time()
+        return t2 - t1
+    def both(self, iterations, count):
+        correct = count * (count + 1) // 2
+        t1 = time.time()
+        for i in range(iterations):
+            res = consume_chain_prolog(e.db.make_chain(count, None)[0])
+            assert res == correct
+        t2 = time.time()
+        return t2 - t1
+    def python(self, iterations, count):
+        correct = count * (count + 1) // 2
+        t1 = time.time()
+        for i in range(iterations):
+            res = consume_chain(make_chain(count))
+            assert res == correct
+        t2 = time.time()
+        return t2 - t1
+
+class PrologLists(Bench):
+    name = "producing and consuming Prolog lists"
+    def prolog(self, iterations, count):
+        t1 = time.time()
+        res = e.db.make_and_consume_list_n_times(iterations, count)
+        assert res == ()
+        t2 = time.time()
+        return t2 - t1
+    def both(self, iterations, count):
+        correct = count * (count + 1) // 2
+        t1 = time.time()
+        for i in range(iterations):
+            res = consume_list(e.db.make_list(count, None)[0])
+            assert res == correct
+        t2 = time.time()
+        return t2 - t1
+    def python(self, iterations, count):
+        correct = count * (count + 1) // 2
+        t1 = time.time()
+        for i in range(iterations):
+            res = consume_list(make_list(count))
+            assert res == correct
+        t2 = time.time()
+        return t2 - t1
+
+class PythonInstances(Bench):
+    name = "producing and consuming instances"
+    def prolog(self, iterations, count):
+        t1 = time.time()
+        e.db.make_and_consume_list_n_times(iterations, count)
+        t2 = time.time()
+        return t2 - t1
+    def both(self, iterations, count):
+        correct = count * (count + 1) // 2
+        t1 = time.time()
+        res = e.db.make_and_consume_instchain_n_times(iterations, count)
+        assert res == ()
+        t2 = time.time()
+        return t2 - t1
+    def python(self, iterations, count):
+        correct = count * (count + 1) // 2
+        t1 = time.time()
+        for i in range(iterations):
+            res = consume_instchain(make_instchain(count))
+            assert res == correct
+        t2 = time.time()
+        return t2 - t1
+
 def main():
     import argparse
 
@@ -144,8 +417,9 @@ def main():
                        help='which benchmark variant to run')
 
     args = parser.parse_args()
+    print "starting"
 
-    for cls in [TinyFunc, Loop1Arg1Result, Loop1Arg0Result]:
+    for cls in BenchMeta.all_classes: #[PythonInstances]: #NondetLoop1Arg1Result, TinyFunc, TinyFuncFromProlog, Loop1Arg1Result, Loop1Arg0Result]:
         cls(args).run_single()
 
 
