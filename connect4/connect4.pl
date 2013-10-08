22df87d69075e71e4991988e8ec0fff0d51b2ff1
Carl Friedrich Bolz <cfbolz@gmx.de>
2013-10-08 19:12:00 +0200

    use whitespace consistently
    four spaces (not tabs, not two spaces)

--- connect4/connect4.pl
+++ connect4/connect4.pl
@@ -10,32 +10,32 @@ direction(v(-1, 1), rldiagnoal).
 
 search_vector(_, _, _, Required, Required).
 search_vector(Coins, c(X, Y), v(Xd, Yd), Required, Ct) :-
-	board_width(W), -1 < X, X < W,
-	board_height(H), -1 < Y, Y < H,
-	member(c(X, Y), Coins),
-	NextCt is Ct + 1,
-	NextX is X + Xd,
-	NextY is Y + Yd,
-	search_vector(Coins, c(NextX, NextY), v(Xd, Yd), Required, NextCt).
+    board_width(W), -1 < X, X < W,
+    board_height(H), -1 < Y, Y < H,
+    member(c(X, Y), Coins),
+    NextCt is Ct + 1,
+    NextX is X + Xd,
+    NextY is Y + Yd,
+    search_vector(Coins, c(NextX, NextY), v(Xd, Yd), Required, NextCt).
 
 find_consecutive(Coins, Required, C) :-
-	direction(Vec, _),
-	search_vector(Coins, C, Vec, Required, 0).
+    direction(Vec, _),
+    search_vector(Coins, C, Vec, Required, 0).
 
 has_won(Reds, _, red) :-
-	member(C, Reds),
-	find_consecutive(Reds, 4, C), !.
+    member(C, Reds),
+    find_consecutive(Reds, 4, C), !.
 
 has_won(_, Yellows, yellow) :-
-	member(C, Yellows),
-	find_consecutive(Yellows, 4, C), !.
+    member(C, Yellows),
+    find_consecutive(Yellows, 4, C), !.
 
 % Test case
 % Note the validity of the board is not checked.
 main(Winner):-
-	Reds = [ c(1, 1), c(2, 2), c(3, 3), c(4, 4) ],
-	Yellows = [ c(3, 4), c(4, 4), c(5, 4), c(6, 4)],
-	has_won(Reds, Yellows, Winner).
+    Reds = [ c(1, 1), c(2, 2), c(3, 3), c(4, 4) ],
+    Yellows = [ c(3, 4), c(4, 4), c(5, 4), c(6, 4)],
+    has_won(Reds, Yellows, Winner).
 
 % ---/// Stuff for the minimax solver ///---
 
@@ -45,72 +45,72 @@ main(Winner):-
 % Collects the cost of the game with respect to the incoming pos
 % Note that the board state is not checked to be valid.
 staticval(pos(RedCounters, YellowCounters, _), WinVal) :-
-	has_won(RedCounters, YellowCounters, WhoWon), !,
-	(WhoWon = red -> WinVal = -99999; WinVal = 99999), !. % heavy weights for win
+    has_won(RedCounters, YellowCounters, WhoWon), !,
+    (WhoWon = red -> WinVal = -99999; WinVal = 99999), !. % heavy weights for win
 
 staticval(pos(RedCounters, YellowCounters, _), Val) :-
-	staticval_player(RedCounters, RedCounters, ValRed),
-	staticval_player(YellowCounters, YellowCounters, ValYellow),
-	Val is ValYellow - ValRed.
+    staticval_player(RedCounters, RedCounters, ValRed),
+    staticval_player(YellowCounters, YellowCounters, ValYellow),
+    Val is ValYellow - ValRed.
 
 % Collects the score of a single player's counters.
 staticval_player(_, [], 0). % No work left, we are done.
 staticval_player(OnePlayersCounters, [WorkCounter | OtherWorkCounters], Val) :-
-	staticval_counter(OnePlayersCounters, WorkCounter, CounterVal),
-	staticval_player(OnePlayersCounters, OtherWorkCounters, OtherCounterVals),
-	Val is CounterVal + OtherCounterVals.
+    staticval_counter(OnePlayersCounters, WorkCounter, CounterVal),
+    staticval_player(OnePlayersCounters, OtherWorkCounters, OtherCounterVals),
+    Val is CounterVal + OtherCounterVals.
 
 % Collects the score of a single counter
 staticval_counter(OnePlayersCounters, WorkCounter, CounterVal) :-
-	findall(1, find_consecutive(OnePlayersCounters, 2, WorkCounter), List),
-	length(List, CounterVal).
+    findall(1, find_consecutive(OnePlayersCounters, 2, WorkCounter), List),
+    length(List, CounterVal).
 
 % Finds how high a token would sit when inserted
 get_insert_y(Toks, Col, YVal) :-
-	findall(Y, member(c(Col, Y), Toks), Ys),
-	(length(Ys, 0) -> (
+    findall(Y, member(c(Col, Y), Toks), Ys),
+    (length(Ys, 0) -> (
         board_height(H),
         YVal is H - 1
-	); (
-		min_member(TopY, Ys),
+    ); (
+        min_member(TopY, Ys),
         YVal is TopY - 1
-	)).
+    )).
 
 % Computes the new board state should we insert a token
 insert_token(pos(Reds, Yellows, WhoseMove), Col, Move) :-
-	append(Reds, Yellows, AllToks),
-	get_insert_y(AllToks, Col, Y),
-	(WhoseMove = red -> (
-		append(Reds, [c(Col, Y)], NewReds),
-		Move = pos(NewReds, Yellows, yellow)
-	) ; (
-		append(Yellows, [c(Col, Y)], NewYellows),
-		Move = pos(Reds, NewYellows, red)
-	)).
+    append(Reds, Yellows, AllToks),
+    get_insert_y(AllToks, Col, Y),
+    (WhoseMove = red -> (
+        append(Reds, [c(Col, Y)], NewReds),
+        Move = pos(NewReds, Yellows, yellow)
+    ) ; (
+        append(Yellows, [c(Col, Y)], NewYellows),
+        Move = pos(Reds, NewYellows, red)
+    )).
 
 % Find all possible subsequent game states
 moves(pos(Reds, Yellows, _), []) :- % if someone won, dont collect moves
-	has_won(Reds, Yellows, _), !.
+    has_won(Reds, Yellows, _), !.
 moves(Pos, Moves) :-
-	board_width(Width), Col is Width - 1,
-	findall(Move, moves(Pos, Move, Col), Moves).
+    board_width(Width), Col is Width - 1,
+    findall(Move, moves(Pos, Move, Col), Moves).
 
 moves(pos(Reds, Yellows, WhoseMove), Move, Col) :-
-	board_width(W), Col < W, Col > -1,
+    board_width(W), Col < W, Col > -1,
     %board_height(H), TopSlot is H - 1,
-	\+ member(c(Col, 0), Reds),
-	\+ member(c(Col, 0), Yellows),
-	insert_token(pos(Reds, Yellows, WhoseMove), Col, Move). % space in this col
+    \+ member(c(Col, 0), Reds),
+    \+ member(c(Col, 0), Yellows),
+    insert_token(pos(Reds, Yellows, WhoseMove), Col, Move). % space in this col
 moves(pos(Reds, Yellows, WhoseMove), Move, Col) :-
-	Col > -1, NextCol is Col - 1,
-	moves(pos(Reds, Yellows, WhoseMove), Move, NextCol). % search next col
+    Col > -1, NextCol is Col - 1,
+    moves(pos(Reds, Yellows, WhoseMove), Move, NextCol). % search next col
 
 min_to_move(pos(_, _, red)).
 max_to_move(pos(_, _, yellow)).
-	
+
 % Just testing
 test(GoodPos, Val) :-
-	Reds = [c(0, 0), c(0, 1), c(0, 2)],
-	Yellows = [],
-	Pos = pos(Reds, Yellows, red),
-	alphabeta(Pos, -99999, 99999, GoodPos, Val, 6).
+    Reds = [c(0, 0), c(0, 1), c(0, 2)],
+    Yellows = [],
+    Pos = pos(Reds, Yellows, red),
+    alphabeta(Pos, -99999, 99999, GoodPos, Val, 6).
--- connect4/minimax.pl
+++ connect4/minimax.pl
@@ -7,46 +7,47 @@
 
 :- module(minimax, [alphabeta/6]).
 
-alphabeta( Pos, Alpha, Beta, GoodPos, Val, Depth)  :-
-  user:moves( Pos, PosList), !,
-  Depth0 is Depth - 1,
-  alphabetamoves(PosList, Pos, Alpha, Beta, GoodPos, Val, Depth0).
+alphabeta(Pos, Alpha, Beta, GoodPos, Val, Depth)  :-
+    user:moves(Pos, PosList), !,
+    Depth0 is Depth - 1,
+    alphabetamoves(PosList, Pos, Alpha, Beta, GoodPos, Val, Depth0).
 
 alphabetamoves([], Pos, _, _, _, Val, _)  :-
-  user:staticval(Pos, Val), !.                              % Static value of Pos 
+    user:staticval(Pos, Val), !.                              % Static value of Pos
 
 alphabetamoves(PosList, Pos, Alpha, Beta, GoodPos, Val, Depth)  :-
-  Depth =< 0 ->
-  user:staticval(Pos, Val);
-  boundedbest( PosList, Alpha, Beta, GoodPos, Val, Depth).
+    Depth =< 0 ->
+        user:staticval(Pos, Val)
+    ;
+        boundedbest(PosList, Alpha, Beta, GoodPos, Val, Depth).
 
-boundedbest( [Pos | PosList], Alpha, Beta, GoodPos, GoodVal, Depth)  :-
-  alphabeta( Pos, Alpha, Beta, _, Val, Depth),
-  goodenough( PosList, Alpha, Beta, Pos, Val, GoodPos, GoodVal, Depth).
+boundedbest([Pos | PosList], Alpha, Beta, GoodPos, GoodVal, Depth)  :-
+    alphabeta(Pos, Alpha, Beta, _, Val, Depth),
+    goodenough(PosList, Alpha, Beta, Pos, Val, GoodPos, GoodVal, Depth).
 
-goodenough( [], _, _, Pos, Val, Pos, Val, _)  :-  !.    % No other candidate
+goodenough([], _, _, Pos, Val, Pos, Val, _)  :-  !.    % No other candidate
 
-goodenough( _, Alpha, Beta, Pos, Val, Pos, Val, _)  :-
-  user:min_to_move( Pos), Val > Beta, !                   % Maximizer attained upper bound
-  ;
-  user:max_to_move( Pos), Val < Alpha, !.                 % Minimizer attained lower bound
+goodenough(_, Alpha, Beta, Pos, Val, Pos, Val, _)  :-
+    user:min_to_move(Pos), Val > Beta, !                   % Maximizer attained upper bound
+    ;
+    user:max_to_move(Pos), Val < Alpha, !.                 % Minimizer attained lower bound
 
-goodenough( PosList, Alpha, Beta, Pos, Val, GoodPos, GoodVal, Depth)  :-
-  newbounds( Alpha, Beta, Pos, Val, NewAlpha, NewBeta),    % Refine bounds  
-  boundedbest( PosList, NewAlpha, NewBeta, Pos1, Val1, Depth),
-  betterof( Pos, Val, Pos1, Val1, GoodPos, GoodVal).
+goodenough(PosList, Alpha, Beta, Pos, Val, GoodPos, GoodVal, Depth)  :-
+    newbounds(Alpha, Beta, Pos, Val, NewAlpha, NewBeta),    % Refine bounds
+    boundedbest(PosList, NewAlpha, NewBeta, Pos1, Val1, Depth),
+    betterof(Pos, Val, Pos1, Val1, GoodPos, GoodVal).
 
-newbounds( Alpha, Beta, Pos, Val, Val, Beta)  :-
-  user:min_to_move( Pos), Val > Alpha, !.                 % Maximizer increased lower bound 
+newbounds(Alpha, Beta, Pos, Val, Val, Beta)  :-
+    user:min_to_move(Pos), Val > Alpha, !.                 % Maximizer increased lower bound
 
-newbounds( Alpha, Beta, Pos, Val, Alpha, Val)  :-
-   user:max_to_move( Pos), Val < Beta, !.                 % Minimizer decreased upper bound 
+newbounds(Alpha, Beta, Pos, Val, Alpha, Val)  :-
+     user:max_to_move(Pos), Val < Beta, !.                 % Minimizer decreased upper bound
 
-newbounds( Alpha, Beta, _, _, Alpha, Beta).          % Otherwise bounds unchanged 
+newbounds(Alpha, Beta, _, _, Alpha, Beta).          % Otherwise bounds unchanged
 
-betterof( Pos, Val, _Pos1, Val1, Pos, Val)  :-        % Pos better than Pos1 
-  user:min_to_move( Pos), Val > Val1, !
-  ;
-  user:max_to_move( Pos), Val < Val1, !.
+betterof(Pos, Val, _Pos1, Val1, Pos, Val)  :-        % Pos better than Pos1
+    user:min_to_move(Pos), Val > Val1, !
+    ;
+    user:max_to_move(Pos), Val < Val1, !.
 
-betterof( _, _, Pos1, Val1, Pos1, Val1).             % Otherwise Pos1 better
+betterof(_, _, Pos1, Val1, Pos1, Val1).             % Otherwise Pos1 better
